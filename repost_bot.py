4102f61a24ce04b2b57826696e8d11937352b708
Bill Gan <BillyG@Bills-MacBook-Pro.local>
2015-12-31 13:12:49 -0500

    initial commit. Added basic layout of repost_bot.py, a fews links for each regex

---
+++ repost_bot.py
@@ -0,0 +1,26 @@
+import praw
+import re
+
+OSAPREG = ""
+PEYREG = ""
+COLLEGEREG = ""
+
+
+reg_to_links =
+{
+OSAPREG: [],
+PEYREG: [],
+COLLEGEREG: []
+}
+
+def get_links_from_file():
+    pass
+
+def main():
+    return
+
+
+
+if __name__ == "__main__":
+    while True:
+        main()
